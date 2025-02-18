9012047d6b37812ddfce03d70233d40d58dfd3ba
Aaron Dweck <aarondweck24@gmail.com>
2025-02-18 12:38:22 +0000

    finished level up content

--- exercises.py
+++ exercises.py
@@ -23,7 +23,7 @@ print('Exercise 1:', manage_students())
 # Create a variable named meal and assign an empty string to it.
 # Use a for loop to iterate over the strings in foods and append each string to meal.
 
-foods = ('first food', 'second food', 'third food')
+foods = ('first food with an a', 'second food', 'third food also has an a')
 
 def combine_foods():
     # your code here
@@ -91,3 +91,33 @@ def list_home_town_items():
 
 # Call the function and print the result
 print('Exercise 5:', list_home_town_items())
+
+# Exercise 6: Celebrate Students
+#
+# Using the list of students and a list comprehension, assign to a variable named awesome_students a new list containing strings.
+# For example: ["Tina is awesome!", "Fred is awesome!", "Wilma is awesome!"]
+
+def create_awesome_students():
+    # your code here
+
+    awesome_students = [student + ' is awesome!' for student in students]
+
+    return awesome_students
+
+# Call the function and print the result
+print('Exercise 6:', create_awesome_students())
+
+# Exercise 7: Filter Foods
+#
+# Assign to a variable named foods_with_an_a the result of list comprehension that filters the foods tuple to only include food strings that contain the letter 'a'.
+# For example, if foods is a tuple of ('Taco', 'Burrito', 'Sandwich'), foods_with_an_a would be a list equal to ['Taco', 'Sandwich']
+
+def filter_foods_with_a():
+    # your code here
+
+    foods_with_an_a = [food for food in foods if 'a' in food.lower()]
+
+    return foods_with_an_a
+
+# Call the function and print the result
+print('Exercise 7:', filter_foods_with_a())
